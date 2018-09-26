91eeced00d9184b44c6541e7eeef751a4af80e2e
Victor Gardrinier <v.gardrinier@gmail.com>
2018-09-26 00:27:56 -0400

    updated sschema but I did not change it

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180920021004) do
+ActiveRecord::Schema.define(version: 20180926042209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,10 +26,9 @@ ActiveRecord::Schema.define(version: 20180920021004) do
   end
 
   create_table "companies", force: :cascade do |t|
-    t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
-  add_foreign_key "articles", "companies"
 end
