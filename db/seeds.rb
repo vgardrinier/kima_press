8007afb3d1e2e2c1bc6a8910454bfef22a8016eb
Victor Gardrinier <v.gardrinier@gmail.com>
2018-10-14 21:31:40 -0400

    populate db using faker in the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+last_company = Company.count
+last_member = Member.count
+rand_company = (1...last_company)
+rand_member = (1...last_member)
+
+20 times do
+  Member.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    username: Faker::OnePiece.character,
+    company_id: rand_company
+    )
+end
+
+20 times do
+  Article.create(
+    title: Faker::Book.title,
+    date: Faker::Date.between(600.days.ago, Date.today),
+    member_id: rand_member
+    )
+end
