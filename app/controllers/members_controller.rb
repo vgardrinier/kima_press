5c9e9ff0304f25ac146e90e7ffa09b885c22262d
Victor Gardrinier <v.gardrinier@gmail.com>
2018-10-14 20:11:39 -0400

    create controller for members with methods

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,42 @@
+class MembersController < ApplicationController
+  def index
+    @members = Member.all
+  end
+
+  def show
+    @member = Member.find(params[:id])
+  end
+
+  def new
+    @member = Member.new
+  end
+
+  def create
+    @member = Member.new(member_params)
+    if @member.save
+      redirect_to members_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @member = Member.find(params[:id])
+  end
+
+  def update
+    @member = Member.find(params[:id])
+    @member.update
+  end
+
+  def destroy
+    @member = Member.find(params[:id])
+    @member.delete
+  end
+
+  private
+
+  def member_params
+    params.require(:member).permit(:username)
+  end
+end
